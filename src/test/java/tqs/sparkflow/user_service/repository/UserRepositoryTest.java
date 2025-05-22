bc60c8cb868e983c03df767b16837adb304e18cf
Gabriel Silva <gabrielmsilva4@ua.pt>
2025-05-22 02:04:12 +0100

    use testcontainers

--- src/test/java/tqs/sparkflow/user_service/repository/UserRepositoryTest.java
+++ src/test/java/tqs/sparkflow/user_service/repository/UserRepositoryTest.java
@@ -4,6 +4,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import tqs.sparkflow.user_service.model.User;
 
 import java.util.Optional;
@@ -11,7 +16,16 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataMongoTest
+@Testcontainers
 class UserRepositoryTest {
+
+    @Container
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:6.0.2");
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
 
     @Autowired
     private UserRepository userRepository;
