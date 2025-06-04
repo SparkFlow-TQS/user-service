bec80456460835a024432857078caaf998834986
Gabriel Silva <gabrielmsilva4@ua.pt>
2025-06-04 18:32:43 +0100

    improve it

--- src/test/java/tqs/sparkflow/userservice/TestcontainersConfiguration.java
+++ src/test/java/tqs/sparkflow/userservice/TestcontainersConfiguration.java
@@ -1,8 +1,6 @@
 package tqs.sparkflow.userservice;
 
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
@@ -10,19 +8,8 @@ import org.testcontainers.containers.MongoDBContainer;
 @TestConfiguration
 public class TestcontainersConfiguration {
 
-    @Bean
-    @ServiceConnection
-    public MongoDBContainer mongoDBContainer() {
-        return new MongoDBContainer("mongo:6.0.2")
-            .withReuse(true);
-    }
-
     @DynamicPropertySource
-    static void setProperties(DynamicPropertyRegistry registry) {
-        MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:6.0.2")
-            .withReuse(true);
-        mongoDBContainer.start();
-        
+    static void setProperties(DynamicPropertyRegistry registry, MongoDBContainer mongoDBContainer) {
         registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
         registry.add("spring.data.mongodb.database", () -> "test");
         registry.add("spring.data.mongodb.auto-index-creation", () -> true);
--- src/test/java/tqs/sparkflow/userservice/controller/UserControllerIT.java
+++ src/test/java/tqs/sparkflow/userservice/controller/UserControllerIT.java
@@ -4,8 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,12 +11,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.testcontainers.containers.MongoDBContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -41,28 +35,6 @@ import tqs.sparkflow.userservice.repository.UserRepository;
 @ActiveProfiles("test")
 @Testcontainers
 class UserControllerIT {
-
-    @Container
-    private static final MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:6.0.2")
-        .withReuse(true);
-
-    @BeforeAll
-    static void beforeAll() {
-        mongoDBContainer.start();
-    }
-
-    @AfterAll
-    static void afterAll() {
-        if (mongoDBContainer.isRunning()) {
-            mongoDBContainer.stop();
-        }
-    }
-
-    @DynamicPropertySource
-    static void setProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
-        registry.add("spring.data.mongodb.database", () -> "test");
-    }
 
     @Autowired
     private MockMvc mockMvc;
