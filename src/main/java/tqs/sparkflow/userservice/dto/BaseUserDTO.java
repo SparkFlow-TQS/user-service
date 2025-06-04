6518764fa32259208dae4dab7fa587910fbd0eed
Gabriel Silva <gabrielmsilva4@ua.pt>
2025-06-04 23:38:24 +0100

    fix styling issues

--- src/main/java/tqs/sparkflow/userservice/dto/BaseUserDTO.java
+++ src/main/java/tqs/sparkflow/userservice/dto/BaseUserDTO.java
@@ -5,49 +5,49 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public abstract class BaseUserDTO {
-    @NotBlank(message = "Username is required")
-    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
-    private String username;
+  @NotBlank(message = "Username is required")
+  @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
+  private String username;
 
-    @NotBlank(message = "Email is required")
-    @Email(message = "Email should be valid")
-    private String email;
+  @NotBlank(message = "Email is required")
+  @Email(message = "Email should be valid")
+  private String email;
 
-    @NotBlank(message = "Password is required")
-    @Size(min = 6, max = 100, message = "Password must be between 6 and 100 characters")
-    private String password;
+  @NotBlank(message = "Password is required")
+  @Size(min = 6, max = 100, message = "Password must be between 6 and 100 characters")
+  private String password;
 
-    private boolean operator;
+  private boolean operator;
 
-    public String getUsername() {
-        return username;
-    }
+  public String getUsername() {
+    return username;
+  }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+  public void setUsername(String username) {
+    this.username = username;
+  }
 
-    public String getEmail() {
-        return email;
-    }
+  public String getEmail() {
+    return email;
+  }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+  public void setEmail(String email) {
+    this.email = email;
+  }
 
-    public String getPassword() {
-        return password;
-    }
+  public String getPassword() {
+    return password;
+  }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+  public void setPassword(String password) {
+    this.password = password;
+  }
 
-    public boolean isOperator() {
-        return operator;
-    }
+  public boolean isOperator() {
+    return operator;
+  }
 
-    public void setOperator(boolean operator) {
-        this.operator = operator;
-    }
+  public void setOperator(boolean operator) {
+    this.operator = operator;
+  }
 } 
